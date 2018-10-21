1aa960413168c6f587adbfc310a4d76f700bfe85
Adam Gutonski <adamgutons@gmail.com>
2018-10-21 19:13:45 -0400

    api proj. w/ html loader

--- week7_api/api_project/get_json.py
+++ week7_api/api_project/get_json.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+import webbrowser
 
 
 def generateSession():		# generate our web session, url is english wikipedia API
@@ -27,7 +28,7 @@ def generateOldComments(whoToSearch, session, URL):
 		'format': "json",
 		'prop': "revisions",
 		'rvprop': 'user|comment|timestamp',
-		'rvlimit': '20',
+		'rvlimit': '50',
 		'rvdir': 'newer'
 		#'rvstart':'2016-11-09T00:00:00Z',
 		#'rvend':'2016-11-08T00:00:00Z', #year, month, day
@@ -50,7 +51,7 @@ def generateNewComments(whoToSearch, session, URL):
 		'format': "json",
 		'prop': "revisions",
 		'rvprop': 'user|comment|timestamp',
-		'rvlimit': '20',
+		'rvlimit': '50',
 		'rvdir': 'older'
 		#'rvstart':'2016-11-09T00:00:00Z',
 		#'rvend':'2016-11-08T00:00:00Z', #year, month, day
@@ -75,22 +76,22 @@ def gatherComments(fileString):
 	for page in searchingText:
 		revisionList = searchingText[page]['revisions']
 	for revision in revisionList:
-		if revision['comment'] != '':
+		if revision['comment'] != '' and len(revision['comment']) > 1:
 			commentList.append(revision['comment'])
 	return commentList
 
 def buildWebpage(who, oldCommentList, newCommentList):
 	soup = BeautifulSoup('<head></head><body></body>', "lxml") #gets a basic <html><body><div> page started
-	relTag = soup.new_tag("link")
-	relTag.attrs["rel"] = "stylesheet"
+	relTag = soup.new_tag("link") # make a new link tag
+	relTag.attrs["rel"] = "stylesheet" #edit the rel attributes to tell page link is a css stylesheet
 	relTag.attrs["href"] = "webpage.css"
 	soup.head.append(relTag)
 
-	mainDiv = soup.new_tag("div", id="oldComments")
-	soup.body.append(mainDiv)
+	oldCommentsDiv = soup.new_tag("div", id="oldComments") #make a div to contain old comments
+	soup.body.append(oldCommentsDiv)
 	head_tag = soup.new_tag("h1") # create a header tag
-	head_tag.string = who + " old comments" # header tag will be person searched for, plus old or new comments
-	mainDiv.append(head_tag) # put the header tag in our main div class
+	head_tag.string = who + " oldest revision comments" # header tag will be person searched for, plus old or new comments
+	oldCommentsDiv.append(head_tag) # put the header tag in our main div class
 
 	#iterate trhough old comment list, creating a p tag each time and appending the comment text
 	#to the tag.  insert after the first header tag
@@ -101,12 +102,12 @@ def buildWebpage(who, oldCommentList, newCommentList):
 	
 	#create a new div to hold old comments and give it a class slector "oldComments"
 	#append the new div to our soup body
-	newDiv = soup.new_tag("div", id="newComments")
-	soup.body.append(newDiv)
+	newCommentsDiv = soup.new_tag("div", id="newComments")
+	soup.body.append(newCommentsDiv)
 
 	head_tag = soup.new_tag("h1")
-	head_tag.string = who + " new comments"
-	newDiv.append(head_tag)
+	head_tag.string = who + " newest revision comments"
+	newCommentsDiv.append(head_tag)
 
 	for comment in newCommentList:
 		anotherTag = soup.new_tag("p")
@@ -117,6 +118,7 @@ def buildWebpage(who, oldCommentList, newCommentList):
 	print(prettySoup)
 	htmlFile = open('webpage.html', 'w')
 	htmlFile.write(prettySoup)
+	webbrowser.open('webpage.html') #opens our webpage right away...neat!
 	
 def main():
 	session, URL = generateSession()
